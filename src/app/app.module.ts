b60d35bbcf995f3459270e1840ad1242aef9ce96
mourya123-mo <mouryarecharla97@gmail.com>
2025-01-26 17:15:17 +0530

    added pagination ,employee details ,filter ,sort ,delete functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatPaginatorModule} from '@angular/material/paginator';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    HomeComponent
+    HomeComponent,
+    EmployeesComponent,
+    EmployeeDetailsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+    MatTableModule,
+    ReactiveFormsModule,
+    MatPaginatorModule,
+    FormsModule
+    
+  
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/employees/employees.component.ts
@@ -0,0 +1,96 @@
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { OnInit, inject, ChangeDetectorRef } from '@angular/core';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { EmployeesService } from '../employees.service';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css'],
+})
+export class EmployeesComponent implements OnInit, AfterViewInit {
+  employees: any[] = [];
+  term: string = "";
+  limit:number=0;
+  page:number=0;
+  private _liveAnnouncer = inject(LiveAnnouncer);
+  displayedColumns: string[] = [
+    'index',
+    'name',
+    'company',
+    'role',
+    'package',
+    'email',
+    'dob',
+    'address',
+    'hikes',
+    'workMode',
+    'gender',
+    'type',
+    'wifibill',
+    'delete',
+    'veiw',
+  ];
+  constructor(
+    private _employeesService: EmployeesService) {}
+  dataSource = new MatTableDataSource<any>(this.employees);
+ 
+  @ViewChild(MatSort) sort!: MatSort;
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  getEmployees() {
+    this._employeesService.getEmployees().subscribe((data: any) => {
+      this.employees = data;
+      this.dataSource.data = this.employees;
+      console.log(this.employees);
+    }),
+      (err: any) => {
+        alert('internal error');
+      };
+  }
+
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  delete(id: number) {
+    this._employeesService.deleteEmployee(id).subscribe((data: any) => {
+      console.log(data.id);
+      this.getEmployees();
+    });
+  }
+  filter() {
+    this._employeesService
+      .getFilteredEmployee(this.term)
+      .subscribe((data: any) => {
+        this.employees = data;
+        console.log(data);
+        
+      }),
+      (err: any) => {
+        alert('Internal server error');
+      };
+  }
+  pagination(){
+    this._employeesService.getPaginatedEmployees(this.limit,this.page).subscribe(
+      (data:any)=>{
+        console.log(data);
+        this.employees=data;
+      },(err:any)=>{
+        alert('internal error');
+      }
+    ) 
+  }
+}
+
